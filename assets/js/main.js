c0a06f3a1e5418f78da942a11412e295af46255f
juelthpi <juelcse2019@gmail.com>
2025-08-28 05:32:25 +0600

    design update

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,30 @@ jQuery(document).ready(function ($) {
       $navbarText.removeClass("show");
     }
   });
+  // service slider
+  $(".service-slider").slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 3000, // Slide transition duration
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    autoplay: false,
+    autoplaySpeed: 1, // Minimum delay between slides
+    cssEase: "linear", // Smooth easing (no acceleration)
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
